7f7676f3fd85006a946af7306b8093f3d351492a
AntonioValentinSayago <cesar_anto30@hotmail.com>
2024-08-28 22:47:36 -0600

    fix: Update in component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 // import Login from "./components/Login";
 import Productos from "./components/Productos";
+import Navbar from "./helpers/Navbar";
 
 function App() {
 
   return (
     <>
-      
+      <Navbar />
       <Productos />
     </>
   )
